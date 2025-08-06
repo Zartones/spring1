346d73f20acec84a9911622ce0dfc43996198f30
Anton <antchaycka@gmail.com>
2025-08-06 23:22:16 +0200

    Created book entity, BookRepository and BookService

---
+++ BookStore/src/main/java/online/bookstore/model/Book.java
@@ -0,0 +1,29 @@
+package online.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private BigDecimal price;
+
+    private String description;
+
+    private String coverImage;
+
+}
