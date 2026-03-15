d97e73ba0bba38ca6702dd9c82f129e7297cab47
Anton <antchaycka@gmail.com>
2026-03-15 17:16:07 +0100

    added JWT support

---
+++ src/main/java/online/bookstore/dto/user/UserLoginResponseDto.java
@@ -0,0 +1,12 @@
+package online.bookstore.dto.user;
+
+import lombok.Data;
+
+@Data
+public class UserLoginResponseDto {
+    private String token;
+
+    public UserLoginResponseDto(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/online/bookstore/security/AuthenticationService.java
@@ -0,0 +1,27 @@
+package online.bookstore.security;
+
+import lombok.RequiredArgsConstructor;
+import online.bookstore.dto.user.UserLoginRequestDto;
+import online.bookstore.dto.user.UserLoginResponseDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+
+    private final JwUtil jwUtil;
+
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.getEmail(),
+                        requestDto.getPassword())
+        );
+        String token = jwUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
